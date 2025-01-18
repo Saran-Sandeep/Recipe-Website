b3e02cb33cd995ba0285f784b5269f0ab1204a4e
Saran-Sandeep <115928655+Saran-Sandeep@users.noreply.github.com>
2025-01-18 15:31:46 +0530

    Addded contactus.html, fixed navigation

---
+++ pages/common.js
@@ -0,0 +1,16 @@
+const loadHTML = (url, elementId) => {
+  fetch(url)
+    .then((respone) => respone.text())
+    .then((data) => {
+      document.getElementById(elementId).innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error loading the HTML:", error);
+    });
+};
+
+window.onload = () => {
+  console.log("hello");
+  loadHTML("./common/header.html", "header");
+  loadHTML("./common/footer.html", "footer");
+};
